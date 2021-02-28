6ae0d2e10bbed0219b23318b9cf73ed1947f0f7a
Kenen Rauan <rauankenen@gmail.com>
2021-02-28 23:54:06 +0600

    Parsing

---
+++ TSIS4-REGEX/Task1/task1.py
@@ -0,0 +1,34 @@
+import re
+
+text = open("input.txt", 'r', encoding="utf-8")
+data = text.read()
+CompanyNamePattern = r'ТОО \w+'
+BinPattern = r'БИН (\d{12})'
+ItemNamePattern = r'\d+\.\s(.+)'
+ItemAmountPattern = r'(\d),000'
+ItemPrice1Pattern = r'x (\d+\s?\d+,00)'
+ItemPrice2Pattern = r'Стоимость\n(\d+\s?\d+,00)'
+TimePattern = r'Время: (\d\d\.\d\d.\d{4}\s\d\d:\d\d:\d\d)'
+AddressPattern = r'г\..+'
+CompanyName = re.findall(CompanyNamePattern, data)
+Bin = re.findall(BinPattern, data)
+ItemName = re.findall(ItemNamePattern, data)
+ItemAmount = re.findall(ItemAmountPattern, data)
+ItemPrice1 = re.findall(ItemPrice1Pattern, data)
+ItemPrice2 = re.findall(ItemPrice2Pattern, data)
+Time = re.findall(TimePattern, data)
+Address = re.findall(AddressPattern, data)
+
+print(*CompanyName)
+print(*Bin, '\n')
+for i in range(len(ItemName)):
+	print(ItemName[i])
+	print(ItemAmount[i])
+	print(ItemPrice1[i])
+	print(ItemPrice2[i])
+	print('\n')
+
+print(*Time)
+print(*Address)
+
+text.close()
