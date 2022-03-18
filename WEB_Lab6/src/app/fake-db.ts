1c5e9e154131cb512d4de7f1bb449ea81e14d5ff
Kenen Rauan <rauankenen@gmail.com>
2022-03-18 23:04:35 +0600

    .

--- WEB_Lab6/src/app/fake-db.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from "./models";
-
-export const POSTS: Post[] = [
-    {id: 1, title : "title 1", body: "Kbtu is where im studying" },
-    {id: 2, title : "title 2", body: "Kbtu is where im studying" },
-    {id: 3, title : "title 3", body: "Kbtu is where im studying" },
-    {id: 4, title : "title 4", body: "Kbtu is where im studying" },
-]
